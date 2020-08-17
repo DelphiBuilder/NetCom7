b33e396709f39bd5b35355814275cb0fe2c8b5fe
DelphiBuilder <31355724+DelphiBuilder@users.noreply.github.com>
2020-08-17 16:46:02 +0300

    Add files via upload

--- Demos/ChatDemo/Client/ufrmMain.pas
+++ Demos/ChatDemo/Client/ufrmMain.pas
@@ -15,17 +15,13 @@ type
     edtText: TEdit;
     btnSend: TButton;
     Client: TncClientSource;
-    tmrUpdateLog: TTimer;
     edtClientName: TEdit;
     ToolBar1: TToolBar;
     btnActivateClient: TButton;
     edtHost: TEdit;
-    procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure btnSendClick(Sender: TObject);
-    procedure tmrUpdateLogTimer(Sender: TObject);
-    function ClientHandleCommand(Sender: TObject; aLine: TncLine; aCmd: Integer;
-      const aData: TArray<System.Byte>; aRequiresResult: Boolean;
+    function ClientHandleCommand(Sender: TObject; aLine: TncLine; aCmd: Integer; const aData: TArray<System.Byte>; aRequiresResult: Boolean;
       const aSenderComponent, aReceiverComponent: string): TArray<System.Byte>;
     procedure edtTextEnter(Sender: TObject);
     procedure edtTextExit(Sender: TObject);
@@ -35,8 +31,6 @@ type
     procedure ClientReconnected(Sender: TObject; aLine: TncLine);
     procedure edtHostChange(Sender: TObject);
   private
-    LogLock: TCriticalSection;
-    LogLines, LogLinesCopy: TStringList;
   public
     procedure Log(aStr: string);
   end;
@@ -48,52 +42,20 @@ implementation
 
 {$R *.fmx}
 
-procedure TfrmMain.FormCreate(Sender: TObject);
-begin
-  LogLock := TCriticalSection.Create;
-  LogLines := TStringList.Create;
-  LogLinesCopy := TStringList.Create;
-end;
-
 procedure TfrmMain.FormDestroy(Sender: TObject);
 begin
   Client.Active := False;
-
-  LogLinesCopy.Free;
-  LogLines.Free;
-  LogLock.Free;
 end;
 
 procedure TfrmMain.Log(aStr: string);
 begin
   // This is thread safe
-  LogLock.Acquire;
-  try
-    LogLines.Add (aStr);
-  finally
-    LogLock.Release;
-  end;
-end;
-
-procedure TfrmMain.tmrUpdateLogTimer(Sender: TObject);
-var
-  i: Integer;
-begin
-  // Update the memLog from LogLines
-  LogLock.Acquire;
-  try
-    LogLinesCopy.Assign(LogLines);
-    LogLines.Clear;
-  finally
-    LogLock.Release;
-  end;
-
-  for i := 0 to LogLinesCopy.Count - 1 do
-  begin
-    memLog.Lines.Add(LogLinesCopy.Strings[i]);
-    memLog.SelStart := Length(memLog.Text);
-  end;
-  LogLinesCopy.Clear;
+  TThread.Queue(nil,
+    procedure
+    begin
+      memLog.Lines.Add(aStr);
+      memLog.ScrollBy(0, 100);
+    end);
 end;
 
 procedure TfrmMain.btnActivateClientClick(Sender: TObject);
@@ -103,14 +65,13 @@ end;
 
 procedure TfrmMain.btnSendClick(Sender: TObject);
 begin
-  Client.ExecCommand(0, BytesOf (edtClientName.Text + ': ' + edtText.Text));
+  Client.ExecCommand(0, BytesOf(edtClientName.Text + ': ' + edtText.Text));
   edtText.Text := '';
 end;
 
 procedure TfrmMain.edtHostChange(Sender: TObject);
 begin
-  if not Client.Active then
-    Client.Host := edtHost.Text;
+  Client.Host := edtHost.Text;
 end;
 
 procedure TfrmMain.edtTextEnter(Sender: TObject);
@@ -125,34 +86,33 @@ end;
 
 procedure TfrmMain.ClientConnected(Sender: TObject; aLine: TncLine);
 begin
-  Log ('Client connected to peer: ' + aLine.PeerIP);
-  TThread.Synchronize(nil,
-  procedure
-  begin
-    btnActivateClient.Text := 'Deactivate client';
-  end);
+  Log('Client connected to peer: ' + aLine.PeerIP);
+  TThread.Queue(nil,
+    procedure
+    begin
+      btnActivateClient.Text := 'Deactivate client';
+    end);
 end;
 
 procedure TfrmMain.ClientDisconnected(Sender: TObject; aLine: TncLine);
 begin
-  Log ('Client disconnected from peer: ' + aLine.PeerIP);
-  TThread.Synchronize(nil,
-  procedure
-  begin
-    btnActivateClient.Text := 'Activate client';
-  end);
+  Log('Client disconnected from peer: ' + aLine.PeerIP);
+  TThread.Queue(nil,
+    procedure
+    begin
+      btnActivateClient.Text := 'Activate client';
+    end);
 end;
 
 procedure TfrmMain.ClientReconnected(Sender: TObject; aLine: TncLine);
 begin
-  Log ('Client was reconnected to peer: ' + aLine.PeerIP);
+  Log('Client was reconnected to peer: ' + aLine.PeerIP);
 end;
 
-function TfrmMain.ClientHandleCommand(Sender: TObject; aLine: TncLine;
-  aCmd: Integer; const aData: TArray<System.Byte>; aRequiresResult: Boolean;
-  const aSenderComponent, aReceiverComponent: string): TArray<System.Byte>;
+function TfrmMain.ClientHandleCommand(Sender: TObject; aLine: TncLine; aCmd: Integer; const aData: TArray<System.Byte>; aRequiresResult: Boolean;
+const aSenderComponent, aReceiverComponent: string): TArray<System.Byte>;
 begin
-  Log (Stringof (aData));
+  Log(Stringof(aData));
 end;
 
 end.
